44f6ecc27fe9fdf3ed096e75a16bd21b9e446c82
decade <648662924@qq.com>
2020-11-12 09:13:03 +0800

    initialize springboot project

---
+++ mall-ums/src/main/java/com/decade/mall/ums/MallUmsApplication.java
@@ -0,0 +1,13 @@
+package com.decade.mall.ums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallUmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallUmsApplication.class, args);
+    }
+
+}
